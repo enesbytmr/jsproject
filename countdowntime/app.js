d4225d6fa44f7e12168f0bf5aa2ace85ae4f415e
enes baytemir <enesbytmr@gmail.com>
2022-04-08 23:59:32 +0300

    update

---
+++ countdowntime/app.js
@@ -0,0 +1,29 @@
+
+const _ = require("lodash")
+
+//buttons 
+let stopButton = document.addEventListener('stopButton')
+let startButton = document.addEventListener('StartButton')
+let resetButton = document.addEventListener('resetButton')
+let lapButton = document.addEventListener('lapButton');
+
+
+//Countdown
+let seconds= 0
+let miliSeconds= 0
+
+let countdownTimer =()=>{
+
+}
+
+// start 
+
+// stop
+
+//reset
+
+// save
+
+//table => add table elements 
+
+//
