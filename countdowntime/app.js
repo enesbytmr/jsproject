8fdc38792a2ccd26f99e919d1f698ac625b6cd83
enes baytemir <enesbytmr@gmail.com>
2022-04-09 13:00:23 +0300

    finished

--- countdowntime/app.js
+++ countdowntime/app.js
@@ -1,109 +1,94 @@
-
-
-//buttons 
-const stopButton = document.getElementById('stopButton')
-const startButton = document.getElementById('startButton')
-const resetButton = document.getElementById('resetButton')
-const lapButton = document.getElementById('lapButton');
-const timer = document.getElementById('stopWatch')
-const addTable = document.getElementById('addTable')
+//buttons
+const stopButton = document.getElementById("stopButton");
+const startButton = document.getElementById("startButton");
+const resetButton = document.getElementById("resetButton");
+const lapButton = document.getElementById("lapButton");
+const timer = document.getElementById("stopWatch");
+const addTable = document.getElementById("addTable");
 
 //stopwatch requirements
 
-
 let stopTime = true;
-let count =0 ;
-let sec = 0
-let min =0
-let hr = 0
+let count = 0;
+let sec = 00;
+let min = 00;
+let hr = 00;
 
-
-
-// start 
-let startTimer = () =>{
-
-    if(stopTime == true){
-        stopTime = false ;
-        timerCycle();
-    }
-}
+// start
+let startTimer = () => {
+  if (stopTime == true) {
+    stopTime = false;
+    timerCycle();
+  }
+};
 
 // stop
-let stopTimer = ()=>{
-    if(stopTime == false){
-        stopTime = true;
-    }
-}
+let stopTimer = () => {
+  if (stopTime == false) {
+    stopTime = true;
+  }
+};
 
 //reset
 let resetTimer = () => {
-    sec= 0 
-min = 0 
-hr = 0 
-    timer.innerHTML = '00:00:00'
-    stopTime = true
-
-}
-
+  sec = 00;
+  min = 00;
+  hr = 00;
+  timer.innerHTML = "00:00:00";
+  stopTime = true;
+};
 
 // Lap
-//table => add table elements 
+//table => add table elements
 
-let counter = ()=>{
-    count += 1
-}
-let lapNumber = lapButton.addEventListener('click',counter)
-let lapTime =() => {
-   let lapNow = `<tr><th scope="row">Lap ${count}</th><td>${hr} : ${min} : ${sec} </td><td></td></tr>`;
-    addTable.innerHTML += lapNow;
-  }
-  let timerCycle = ()=> {
-    if (stopTime == false) {
-        
-        sec = parseInt(sec);
-        min = parseInt(min);
-        hr = parseInt(hr);
-    
-        sec = sec + 1;
-    
-        if (sec == 60) {
-          min = min + 1;
-          sec = 0;
-        }
-        if (min == 60) {
-          hr = hr + 1;
-          min = 0;
-          sec = 0;
-        }
-    
-        if (sec < 10 || sec == 0) {
-          sec = '0' + sec;
-        }
-        if (min < 10 || min == 0) {
-          min = '0' + min;
-        }
-        if (hr < 10 || hr == 0) {
-          hr = '0' + hr;
-        }
-    
-         timer.innerHTML = hr + ':' + min + ':' + sec;
-    
-        setTimeout("timerCycle()", 1000);
-      }
+let counter = () => {
+  count += 1;
+};
+let lapNumber = lapButton.addEventListener("click", counter);
+let lapTime = () => {
+  let lapNow = `<tr><th scope="row">Lap ${count}</th><td>${hr} : ${min} : ${sec} </td><td></td></tr>`;
+  addTable.innerHTML += lapNow;
+};
+let timerCycle = () => {
+  if (stopTime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    hr = parseInt(hr);
+
+    sec = sec + 1;
+
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
     }
 
+    if (sec < 10 || sec == 0) {
+      sec = "0" + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = "0" + min;
+    }
+    if (hr < 10 || hr == 0) {
+      hr = "0" + hr;
+    }
 
+    timer.innerHTML = hr + ":" + min + ":" + sec;
 
-
-
-
+    setTimeout("timerCycle()", 1000);
+  }
+};
 
 //
 //start
-startButton.addEventListener('click',startTimer)
+startButton.addEventListener("click", startTimer);
 //stop
-stopButton.addEventListener('click',stopTimer)
+stopButton.addEventListener("click", stopTimer);
 //reset
-resetButton.addEventListener('click',resetTimer)
+resetButton.addEventListener("click", resetTimer);
 //lap
-lapButton.addEventListener('click',lapTime)
+lapButton.addEventListener("click", lapTime);
