2e5d5f86d29c0a6f0504565f0062784ae150860f
enes baytemir <enesbytmr@gmail.com>
2022-04-11 02:32:50 +0300

    finished design(html with bootstrap) and tasks added to app.js

---
+++ randomQuote/app.js
@@ -0,0 +1,14 @@
+//add sources anxious 
+
+//define quote author btn
+const quote = document.getElementById("quote")
+const author = document.getElementById("author")
+const btn = document.getElementById("btn")
+
+
+//talking with api 
+
+
+
+
+//dom 
