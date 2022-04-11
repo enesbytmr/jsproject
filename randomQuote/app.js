c16403d5493f9e20f841247cc66e65dc3d011190
enes baytemir <enesbytmr@gmail.com>
2022-04-11 03:05:22 +0300

    update

--- randomQuote/app.js
+++ randomQuote/app.js
@@ -1,4 +1,28 @@
-//add sources anxious 
+//PSEUDO
+// anxious talk with api 
+// sent api req random quote
+// get api responce random quote
+// json control 
+// json to dom 
+// when u click generate new quote
+
+
+//add sources axios express fs http 
+const axios = require('axios')
+const express = require('express')
+const fs = require('fs')
+const http = require('http')
+//server up
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 //define quote author btn
 const quote = document.getElementById("quote")
@@ -12,3 +36,5 @@ const btn = document.getElementById("btn")
 
 
 //dom 
+
+//
