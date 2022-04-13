a6c5c83fe392a2c7b64c708722135dd019eacf86
enes baytemir <enesbytmr@gmail.com>
2022-04-13 23:24:43 +0300

    jsdom

--- randomQuote/app.js
+++ randomQuote/app.js
@@ -1,5 +1,5 @@
 //PSEUDO
-// anxious talk with api 
+// axios talk with api 
 // sent api req random quote
 // get api responce random quote
 // json control 
@@ -8,28 +8,46 @@
 
 
 //add sources axios express fs http 
+let contentStr,content;
 const axios = require('axios')
 const express = require('express')
 const fs = require('fs')
 const http = require('http')
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const html = fs.readFile('index.html', function(err, data) {
+  if(err){
+    console.log(err)
+  }else{
+     contentStr = data.toString();
+     content = data;
+  }
+ });
+
+const dom = new JSDOM(contentStr);
+const dom1= new JSDOM(content)
+
 //server up
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(contentStr)
 })
 
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 //define quote author btn
-const quote = document.getElementById("quote")
-const author = document.getElementById("author")
-const btn = document.getElementById("btn")
+//quote author btn 
+const document = dom1.window.document;
+const author= document.querySelector('#author')
+const quote = document.querySelector('#quote')
+const btn = document.querySelector('#btn')
 
-
+console.log(author)
+//  
 //talking with api 
 
 
